980083aa31cfec9371ccc2084fa9bde974eda0eb
sarcoma <sarcoma@live.co.uk>
2019-05-07 00:31:31 +0100

    remove webpack, add preppy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,4 @@ import JsonResponse from './json-response';
 import ResponseType from './response-type';
 
 export { makeRoute, makeUrl, makeEndpoint, makeParams } from './url';
-export { Fetch, JsonResponse, ResponseType };
-
-export default Request;
+export { Request, Fetch, JsonResponse, ResponseType };
